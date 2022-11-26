a757b54816edd5255911456bbd8c30dd56a55678
Klaus Almeida <74985395+ttercept2@users.noreply.github.com>
2022-11-26 16:52:57 -0300

    Add files via upload

--- Fase.java
+++ Fase.java
@@ -6,13 +6,17 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 public class Fase extends JPanel{
     private Image fundo;
+    private Image chao;
     public Fase(){
         ImageIcon referencia = new ImageIcon("imagens\\konoha.jpg");
         fundo = referencia.getImage();
+        ImageIcon referencia2 = new ImageIcon("imagens\\layer.png");
+        chao = referencia2.getImage();
     }
     public void paint(Graphics g){
         Graphics2D graficos = (Graphics2D) g;
         graficos.drawImage(fundo,-100,0,null);
+        graficos.drawImage(chao,0,-220,null);
         g.dispose();
     }
 }
